2b43b4b06ada192331b0daba9a407f6e0e6f4c5c
ELCRND <jwhh2a@gmail.com>
2024-02-13 14:38:52 +0300

    new file:   assets/CD_Project_Red.png 	deleted:    assets/CD_Project_Red.svg 	new file:   assets/PC_logo.png 	deleted:    assets/PC_logo.svg 	modified:   assets/PS4_logo.png 	renamed:    assets/Playstation-5.png -> assets/Playstation_5.png 	new file:   assets/X_PS.png 	renamed:    assets/cd_disks.jpeg -> assets/cd.jpeg 	deleted:    assets/cd_disk_icon.svg 	new file:   assets/cd_icon.svg 	deleted:    assets/fonts/Archangelsk.woff2 	deleted:    assets/fonts/ProtestRiot-Regular.ttf 	new file:   assets/hero_bg_1_mobile.png 	new file:   assets/hero_bg_2_mobile.png 	new file:   assets/hero_bg_3_mobile.png 	modified:   assets/img_icon.svg 	modified:   assets/list_icon.svg 	deleted:    assets/prizes_bg.png 	new file:   assets/promotion_after.png 	deleted:    assets/promotion_after.svg 	modified:   assets/stadia_logo.png 	modified:   css/style.css 	modified:   index.html 	modified:   input.css 	new file:   public/favicon-16x16.png 	new file:   public/favicon-32x32.png 	deleted:    public/vite.svg 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,49 @@ export default {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        title: [
+          "3.875rem",
+          {
+            lineHeight: "4rem",
+            letterSpacing: "-0.01em",
+            fontWeight: "700",
+          },
+        ],
+        regular: [
+          "1.125rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.01em",
+            fontWeight: "600",
+          },
+        ],
+      },
+      colors: {
+        white: "#fff",
+        black: "#000",
+        blue: "#52BEDC",
+        "regular-yellow": "#F8F200",
+        "regular-gray": "#777",
+      },
+      padding: {
+        y: "70px",
+      },
       keyframes: {
         slider: {
           "0%": { backgroundImage: "url('../assets/hero_bg_1.jpeg')" },
           "33%": { backgroundImage: "url('../assets/hero_bg_2.jpeg')" },
           "66%": { backgroundImage: "url('../assets/hero_bg_3.jpeg')" },
         },
+        sliderMobile: {
+          "0%": { backgroundImage: "url('../assets/hero_bg_1_mobile.png')" },
+          "33%": { backgroundImage: "url('../assets/hero_bg_2_mobile.png')" },
+          "66%": { backgroundImage: "url('../assets/hero_bg_3_mobile.png')" },
+        },
       },
       animation: {
         heroSlider: "slider 12s steps(1, end) infinite",
+        heroSliderMobile: "sliderMobile 12s steps(1, end) infinite",
       },
     },
   },
